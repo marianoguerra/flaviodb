4dfcf6ad49250d87bdb1356df3b490826c04fc24
Mariano Guerra <luismarianoguerra@gmail.com>
2014-11-27 11:39:42 +0100

    implemente GET /msgs/:user/:topic?limit=L[&from=F]

--- src/handler_flavio_msgs.erl
+++ src/handler_flavio_msgs.erl
@@ -6,11 +6,20 @@
          rest_terminate/2,
          allowed_methods/2,
          content_types_accepted/2,
+         content_types_provided/2,
          resource_exists/2,
-         from_json/2
+         from_json/2,
+         to_json/2
         ]).
 
--record(state, {username, topic}).
+-record(state, {username, topic, from, limit}).
+
+to_int_or(Bin, Default) ->
+    Str = binary_to_list(Bin),
+    case string:to_integer(Str) of
+        {Int, ""} -> Int;
+        _ -> Default
+    end.
 
 init({tcp, http}, _Req, _Opts) -> {upgrade, protocol, cowboy_rest}.
 
@@ -18,16 +27,29 @@ rest_init(Req, []) ->
     {Username, Req1} = cowboy_req:binding(username, Req),
     {Topic, Req2} = cowboy_req:binding(topic, Req1),
 
-	{ok, Req2, #state{username=Username, topic=Topic}}.
+    {FromStr, Req3} = cowboy_req:qs_val(<<"from">>, Req2, <<"">>),
+    {LimitStr, Req4} = cowboy_req:qs_val(<<"limit">>, Req3, <<"1">>),
 
-allowed_methods(Req, State) -> {[<<"POST">>], Req, State}.
+    From = to_int_or(FromStr, nil),
+    Limit = to_int_or(LimitStr, 1),
+
+	{ok, Req4, #state{username=Username, topic=Topic, from=From, limit=Limit}}.
+
+allowed_methods(Req, State) -> {[<<"POST">>, <<"GET">>], Req, State}.
 
 resource_exists(Req, State) ->
-    Exists = true,
-    {Exists, Req, State}.
+    {Method, Req1} = cowboy_req:method(Req),
+    Exists = case Method of
+                 <<"POST">> -> false;
+                 _ -> true
+             end,
+    {Exists, Req1, State}.
 
 content_types_accepted(Req, State) ->
     {[{{<<"application">>, <<"json">>, '*'}, from_json}], Req, State}.
+
+content_types_provided(Req, State) ->
+    {[{{<<"application">>, <<"json">>, '*'}, to_json}], Req, State}.
 
 from_json(Req, State=#state{username=Username, topic=Topic}) ->
     {ok, Body, Req1} = cowboy_req:body(Req),
@@ -48,6 +70,14 @@ from_json(Req, State=#state{username=Username, topic=Topic}) ->
         false ->
             bad_request(Req1, State, <<"{\"type\": \"invalid-body\"}">>)
     end.
+
+to_json(Req, State=#state{username=Username, topic=Topic, from=From, limit=Limit}) ->
+    {ok, [FirstResponse|_]} = flavio:get_msgs(Username, Topic, From, Limit),
+    {{ok, Entities}, _Partition} = FirstResponse,
+    EntitiesPList = lists:map(fun fixstt:to_proplist/1, Entities),
+    EntitiesJson = jsx:encode(EntitiesPList),
+
+    {EntitiesJson, Req, State}.
 
 rest_terminate(_Req, _State) ->
 	ok.
